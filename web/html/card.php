0ef01163254a68afcfc9178b146b347e2309293e
hrefnick <123642184+hrefnick@users.noreply.github.com>
2023-01-30 11:16:12 -0600

    updated stylesheet link

--- web/html/card.php
+++ web/html/card.php
@@ -39,7 +39,7 @@ $card = mysqli_fetch_array($result, MYSQLI_ASSOC);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../../../../../PHP/final/styles.css" />
+    <link rel="stylesheet" href="styles.css" />
     <title><?= $card['Name'] ?></title>
 </head>
 <div class="cardborder">
